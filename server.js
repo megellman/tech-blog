4957c9b475b45f157a9e1af4279e7fab6a19952a
Megan Ellman <meganlellman@gmail.com>
2023-02-08 18:05:52 -0800

    created form and form scripting

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Import models to sync with the database
 const models = require('./models');
+const hbs = exphbs.create({ helpers: helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
